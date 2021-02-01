502b169f1fe4bfe1ea78775b45a007aa10ebad9b
viazz <shady.sdp@gmail.com>
2021-02-01 10:17:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const TelegramBot = require('node-telegram-bot-api'),
-    token = "905023288:AAHb1cUuwSobjvrrR4eNjJLUYXqFGrLUxhM";//test-bot
+    token = "1470244255:AAFAZ9_XESkk8u6ChG2_nCI6A-KETqhthHA";//test-bot
 
-//const port = process.env.PORT || 3000
 
-const bot = new TelegramBot(token,{polling: true});
+const port = process.env.PORT || 3000
+expressApp.get('/', (req, res) => {
+    res.send('Working!!!')
+})
+expressApp.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+const options = {
+  webHook: {
+    port: port
+  }
+};
+const url_telegram = process.env.APP_URL || 'https://post-spotify-reddit-bots.herokuapp.com:443';
+
+console.log('Script start...')
+
+const bot = new TelegramBot(token, options);//Filmix
+
+bot.setWebHook(`${url_telegram}/bot${token}`);
+
 bot.on('massage',msg => {
     bot.sendMessage(msg.chat.id,'Hi')
     console.log(msg.chat.id)
 })
-//const id = 582743262;
+
